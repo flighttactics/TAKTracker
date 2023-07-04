7412b547629fd71c2985b33f821b76ec0a88e136
Cory Foy <github@cornetdesign.com>
2023-07-03 21:44:41 -0400

    Initial Commit

---
+++ TAK Spike/TAK_SpikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TAK_SpikeApp.swift
+//  TAK Spike
+//
+//  Created by Cory Foy on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TAK_SpikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
