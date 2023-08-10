15b1a4b56633005e821cb76c00be1ce3caaf8c3c
Cory Foy <github@cornetdesign.com>
2023-08-10 15:18:37 -0400

    Adds additional attributes to the CoT message to properly identify us

--- TAKTracker/TAK/COTMessage.swift
+++ TAKTracker/TAK/COTMessage.swift
@@ -8,44 +8,164 @@
 import Foundation
 import MapKit
 
+protocol COTNode {
+    func toXml() -> String
+}
+
+struct COTEvent : COTNode {
+    var version:String
+    var uid:String
+    var type:String
+    var how:String
+    var time:Date
+    var start:Date
+    var stale:Date
+    var childNodes:[COTNode] = []
+    
+    func toXml() -> String {
+        return "<event " +
+        "version='\(version)' " +
+        "uid='\(uid)' " +
+        "type='\(type)' " +
+        "how='\(how)' " +
+        "time='\(ISO8601DateFormatter().string(from: time))' " +
+        "start='\(ISO8601DateFormatter().string(from: start))' " +
+        "stale='\(ISO8601DateFormatter().string(from: stale))'" +
+        ">" +
+        childNodes.map { $0.toXml() }.joined() +
+        "</event>"
+    }
+}
+
+struct COTPoint : COTNode {
+    var lat:String
+    var lon:String
+    var hae:String
+    var ce:String
+    var le:String
+    
+    func toXml() -> String {
+        return "<point " +
+        "lat='\(lat)' " +
+        "lon='\(lon)' " +
+        "hae='\(hae)' " +
+        "ce='\(ce)' " +
+        "le='\(le)'" +
+        "></point>"
+    }
+}
+
+struct COTDetail : COTNode {
+    var childNodes:[COTNode] = []
+    
+    func toXml() -> String {
+        return "<detail>" +
+        childNodes.map { $0.toXml() }.joined() +
+        "</detail>"
+    }
+}
+
+struct COTRemarks : COTNode {
+    
+    func toXml() -> String {
+        return "<remarks></remarks>"
+    }
+}
+
+struct COTGroup : COTNode {
+    var name:String
+    var role:String
+    
+    func toXml() -> String {
+        return "<__group " +
+        "name='\(name)' " +
+        "role='\(role)'" +
+        "></__group>"
+    }
+}
+
+struct COTStatus : COTNode {
+    var battery:String
+    
+    func toXml() -> String {
+        return "<status " +
+        "battery='\(battery)'" +
+        "></status>"
+    }
+}
+
+struct COTTakV : COTNode {
+    var device:String
+    var platform:String
+    var os:String
+    var version:String
+    
+    func toXml() -> String {
+        return "<takv " +
+        "device='\(device)' " +
+        "platform='\(platform)' " +
+        "os='\(os)' " +
+        "version='\(version)'" +
+        "></takv>"
+    }
+}
+
+struct COTTrack : COTNode {
+    var speed:String
+    var course:String
+    
+    func toXml() -> String {
+        return "<track " +
+        "speed='\(speed)' " +
+        "course='\(course)'" +
+        "></track>"
+    }
+}
+
+struct COTContact : COTNode {
+    var endpoint:String = "*:-1:stcp"
+    var phone:String = ""
+    var callsign:String
+    
+    func toXml() -> String {
+        return "<contact " +
+        "endpoint='\(endpoint)' " +
+        "phone='\(phone)' " +
+        "callsign='\(callsign)'" +
+        "></contact>"
+    }
+}
+
+struct COTUid : COTNode {
+    var callsign:String
+    
+    func toXml() -> String {
+        return "<uid " +
+        "Droid='\(callsign)'" +
+        "></uid>"
+    }
+}
+
 class COTMessage: NSObject {
     public func generateCOTXml(location: CLLocation) -> String {
         let cotTimeout = SettingsStore.global.staleTimeMinutes * 60.0
-        let stale = Date().addingTimeInterval(cotTimeout) //needs to be to ISOString Maybe?
-        let staleString = ISO8601DateFormatter().string(from: stale)
-        let nowString = ISO8601DateFormatter().string(from: Date())
+        let deviceID = UIDevice.current.identifierForVendor!.uuidString
         
-        var eventAttributes: [String: String] = [:]
-        eventAttributes["version"] = "2.0"
-        eventAttributes["uid"] = SettingsStore.global.callSign
-        eventAttributes["type"] = SettingsStore.global.cotType
-        eventAttributes["how"] = SettingsStore.global.cotHow
-        eventAttributes["time"] = nowString
-        eventAttributes["start"] = nowString
-        eventAttributes["stale"] = staleString
+        var cotEvent = COTEvent(version: "2.0", uid: deviceID, type: SettingsStore.global.cotType, how: SettingsStore.global.cotHow, time: Date(), start: Date(), stale: Date().addingTimeInterval(cotTimeout))
         
-        var pointAttributes: [String: String] = [:]
-        pointAttributes["lat"] = location.coordinate.latitude.formatted()
-        pointAttributes["lon"] = location.coordinate.longitude.formatted()
-        pointAttributes["hae"] = location.altitude.description
-        pointAttributes["ce"] = "9999999.0"
-        pointAttributes["le"] = "9999999.0"
+        let cotPoint = COTPoint(lat: location.coordinate.latitude.formatted(), lon: location.coordinate.longitude.formatted(), hae: location.altitude.description, ce: "9999999.0", le: "9999999.0")
         
-        let point = "<point " + mapXmlAttrs(nodeAttributes: pointAttributes) + "></point>"
+        var cotDetail = COTDetail()
         
-        let detail = "<detail><contact callsign=\"\(SettingsStore.global.callSign)\" /><remarks></remarks></detail>"
+        cotDetail.childNodes.append(COTContact(callsign: SettingsStore.global.callSign))
+        cotDetail.childNodes.append(COTRemarks())
+        cotDetail.childNodes.append(COTGroup(name: "Cyan", role: "Team Member"))
+        cotDetail.childNodes.append(COTUid(callsign: SettingsStore.global.callSign))
+        cotDetail.childNodes.append(COTTakV(device: "iPhone 14", platform: "iTAK-Tracker-CIV", os: "iOS", version: "1.03"))
         
-        return "<?xml version=\"1.0\" standalone=\"yes\"?>" +
-            "<event " + mapXmlAttrs(nodeAttributes: eventAttributes) + ">" +
-            point +
-            detail +
-            "</event>"
+        cotEvent.childNodes.append(cotPoint)
+        cotEvent.childNodes.append(cotDetail)
+        
+        return "<?xml version=\"1.0\" standalone=\"yes\"?>" + cotEvent.toXml()
     }
-    
-    private func mapXmlAttrs(nodeAttributes:[String: String]) -> String {
-        return nodeAttributes.compactMap( { (key, value) -> String in
-            return "\(key)=\"\(value)\""
-        }).joined(separator: " ")
-    }
-
 }
